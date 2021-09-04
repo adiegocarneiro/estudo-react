4b781302f6975e4637baba2a70d7c15d450f5b35
adiegocarneiro <baal_orienrac@hotmail.com>
2021-09-04 15:14:35 -0300

    Hooks

--- src/Components/TextInput/index.jsx
+++ src/Components/TextInput/index.jsx
@@ -11,3 +11,4 @@ export const TextInput = ( {searchValue, handleChange} ) => {
         />
     )
 }
+
--- src/Templates/Home/index.jsx
+++ src/Templates/Home/index.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react'
+import { useState, useEffect, useCallback } from 'react'
 import { loadPosts } from '../../utils/loadPosts'
 import { Posts } from "../../Components/Posts"
 import { Button } from '../../Components/Button';
@@ -6,7 +6,99 @@ import { Button } from '../../Components/Button';
 import "./estilo.css"
 import { TextInput } from '../../Components/TextInput';
 
-class Home extends Component{
+export const Home = () =>{
+    
+    const [posts, setPosts] = useState([]);
+    const [allPosts, setAllPosts] = useState([]);
+    const [page, setPage] = useState(0);
+    const [postsPerPage] = useState(10);
+    const [searchValue, setSearchValue] = useState('');
+
+    const noMorePosts = page + postsPerPage >= allPosts.length;
+
+    const filteredPosts = !!searchValue ? allPosts.filter(post => {
+        return post.title.toLowerCase().includes(searchValue.toLowerCase())
+    })
+    : posts;
+
+    
+
+    const handleLoadPosts = useCallback(async (page, postsPerPage) => {
+        
+        const posts_zipped_photos = await loadPosts()
+        
+        setPosts(posts_zipped_photos.slice(page,postsPerPage))
+        setAllPosts(posts_zipped_photos)
+    },[])
+
+    useEffect(()=> {
+        handleLoadPosts(0, postsPerPage);
+    },[handleLoadPosts,  postsPerPage])
+    
+    const loadMorePosts = () => {
+        
+        const nextPage = page + postsPerPage
+        const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage)
+
+        posts.push(...nextPosts)//spread operator -> cada elemento de um array vira um parametro
+
+        setPosts(posts);
+        setPage(nextPage);
+
+    }
+
+    const handleChange = (e)=>{
+        const  {value} = e.target;
+        setSearchValue(value)
+    }
+
+    return(
+        <section className="container">
+            <div className="search-container">
+            {//se searchValue true - mostrar search
+                !!searchValue && 
+                (
+                        <h1>Search value:{searchValue}</h1>
+                )
+            }
+                <TextInput 
+                    searchVale = {searchValue}
+                    handleChange = {handleChange}
+                />
+            </div>
+                            
+            {
+                filteredPosts.length > 0 && (
+                    <Posts posts={filteredPosts}/>
+                )
+            }
+
+            {
+                filteredPosts.length === 0 && (
+                    <p>Nenhum post contém "{searchValue}"</p>
+                )
+            }
+
+
+            <div  className="button-container">
+                {   //se searchValue false - esconder botao 
+                    !searchValue && 
+                    (
+                        <Button 
+                            disabled={noMorePosts}
+                            label="Load more posts"
+                            onClick = { loadMorePosts }
+                        />
+                    )
+                }
+            </div>
+        </section>
+    );
+    
+}
+
+/*
+class Home2 extends Component{
     
     state = {
         posts:[],
@@ -15,7 +107,7 @@ class Home extends Component{
         postsPerPage:10,
         searchValue:""
     };
-
+    
 
     loadPosts = async () => {
         const { page, postsPerPage } = this.state
@@ -89,8 +181,7 @@ class Home extends Component{
                         handleChange = {this.handleChange}
                     />
                 </div>
-                
-                
+                                
                 {
                     filteredPosts.length > 0 && (
                         <Posts posts={filteredPosts}/>
@@ -121,5 +212,5 @@ class Home extends Component{
     }
 
 }
-
+*/
 export default Home;
